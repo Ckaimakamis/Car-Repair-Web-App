6ad5f6e4e3326171e361341138020662a31eafc0
Vasilis Kordalis <billkord2012@gmail.com>
2017-10-06 19:07:17 +0300

    First commit

---
+++ src/main/java/com/coding/school/webApp/controller/demoController.java
@@ -0,0 +1,13 @@
+package com.coding.school.webApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class demoController {
+
+    @RequestMapping("/")
+    String demoPage() {
+        return "index";
+    }
+}
