9c676f1dff2bfd8512a7345bdbff0bdf6e9af471
anvailas <anvailas@hotmail.com>
2017-10-08 15:30:39 +0300

    Vehicle class implementation

---
+++ src/main/java/com/coding/school/webApp/model/Vehicle.java
@@ -0,0 +1,36 @@
+package com.coding.school.webApp.model;
+
+public class Vehicle {
+
+    private String plateNumber;
+    private String model;
+    private String year;
+    private String color;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String plateNumber, String model, String year, String color) {
+        this.plateNumber = plateNumber;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    public String getPlateNumber() { return plateNumber; }
+
+    public void setPlateNumber(String plateNumber) { this.plateNumber = plateNumber; }
+
+    public String getModel() { return model; }
+
+    public void setModel(String model) { this.model = model; }
+
+    public String getYear() { return year; }
+
+    public void setYear(String year) { this.year = year; }
+
+    public String getColor() { return color; }
+
+    public void setColor(String color) { this.color = color; }
+
+}
