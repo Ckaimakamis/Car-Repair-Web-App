c34d44b5f23a479466241da39ae0794aab8ff917
Vasilis Kordalis <billkord2012@gmail.com>
2017-10-08 20:07:11 +0300

    Create Repair Controller and Service

--- src/main/java/com/coding/school/webApp/repository/RepairRepository.java
+++ src/main/java/com/coding/school/webApp/repository/RepairRepository.java
@@ -2,7 +2,9 @@ package com.coding.school.webApp.repository;
 
 import com.coding.school.webApp.model.Repair;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("RepairRepository")
 public interface RepairRepository extends JpaRepository<Repair, Integer>{
 
 }
