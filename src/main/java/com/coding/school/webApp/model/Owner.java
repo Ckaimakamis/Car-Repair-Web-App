d811bd2acf6db2b536285cd74ca5515cdc1ce6f3
Ckaimakamis <32458271+Ckaimakamis@users.noreply.github.com>
2017-10-08 16:45:16 +0300

    Δημιουργία κλάσης Owner

---
+++ src/main/java/com/coding/school/webApp/model/Owner.java
@@ -0,0 +1,76 @@
+package com.coding.school.webApp.model;
+
+public class Owner {
+    private int ID;
+    private String firstName;
+    private String lastName;
+    private String vat;
+    private String email;
+    private String password;
+    private OwnerRole role;
+
+    public Owner() {
+    }
+
+    public Owner(int ID, String firstName, String lastName, String vat, String email, String password, OwnerRole role) {
+        this.setID(ID);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setVat(vat);
+        this.setEmail(email);
+        this.setPassword(password);
+        this.setRole(role);
+    }
+
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getVat() {
+        return vat;
+    }
+    public void setVat(String vat) {
+        this.vat = vat;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public OwnerRole getRole() {
+        return role;
+    }
+    public void setRole(OwnerRole role) {
+        this.role = role;
+    }
+
+
+    public enum OwnerRole {admin, user}
+}
