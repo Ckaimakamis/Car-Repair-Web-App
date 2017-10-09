ef6888134b17e830167a711eb9d2f7961afbf8f0
Ckaimakamis <32458271+Ckaimakamis@users.noreply.github.com>
2017-10-09 17:07:57 +0300

    Δημιουργία Owner Controller

--- src/main/java/com/coding/school/webApp/repository/OwnerRepository.java
+++ src/main/java/com/coding/school/webApp/repository/OwnerRepository.java
@@ -4,6 +4,6 @@ import com.coding.school.webApp.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository("RepairRepository")
+@Repository("OwnerRepository")
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 }
