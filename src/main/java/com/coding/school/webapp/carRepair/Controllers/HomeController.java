760f14801a0dfc9218a9010a37f807419c799331
Vasilis Kordalis <billkord2012@gmail.com>
2017-10-16 19:53:03 +0300

    error handling

--- src/main/java/com/coding/school/webapp/carRepair/Controllers/HomeController.java
+++ src/main/java/com/coding/school/webapp/carRepair/Controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.coding.school.webapp.carRepair.Controllers;
 
+import com.coding.school.webapp.carRepair.Model.RegisterForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,11 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class HomeController {
 
+    private static final String REGISTER_FORM = "user";
+
     @RequestMapping(name = "/admin/home", method = RequestMethod.GET)
     String homePage(Model model, HttpSession session){
+        model.addAttribute(REGISTER_FORM, new RegisterForm());
         return "index";
     }
 }
--- src/main/java/com/coding/school/webapp/carRepair/Controllers/RegisterController.java
+++ src/main/java/com/coding/school/webapp/carRepair/Controllers/RegisterController.java
@@ -7,8 +7,10 @@ import com.coding.school.webapp.carRepair.Model.RegisterForm;
 import com.coding.school.webapp.carRepair.Services.OwnerService;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,16 +26,25 @@ public class RegisterController {
     @Autowired
     OwnerService service;
 
+    @Autowired
+    private MessageSource messageSource;
+
     private static final String REGISTER_FORM = "user";
 
     @RequestMapping(name = "/registerUser", method = RequestMethod.POST)
-    String addUser(@Valid @ModelAttribute("REGISTER_FORM") RegisterForm model, HttpSession session,
-                   RedirectAttributes redirectAttributes){
-        try{
-            service.registerOwner(OwnerConverter.buildUserObject(model));
-        }catch (Exception e){
-            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+    String addUser(@Valid @ModelAttribute(REGISTER_FORM) RegisterForm registerForm, BindingResult bindingResult,
+                   HttpSession session, RedirectAttributes redirectAttributes){
+        if (bindingResult.hasErrors()) {
+            String message = messageSource.getMessage(bindingResult.getAllErrors().get(0), null);
+            redirectAttributes.addFlashAttribute("errorMessage", message);
+        }else{
+            try{
+                service.registerOwner(OwnerConverter.buildUserObject(registerForm));
+            }catch (Exception e){
+                redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+            }
         }
+
         return "redirect:/admin/home";
     }
 }
