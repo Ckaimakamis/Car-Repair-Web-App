3698f37c7f33f81b339cef1aca343963e76ebd6b
Ckaimakamis <32458271+Ckaimakamis@users.noreply.github.com>
2017-10-19 19:46:10 +0300

    searchCode

--- src/main/java/com/coding/school/webapp/carRepair/Repositories/RepairRepository.java
+++ src/main/java/com/coding/school/webapp/carRepair/Repositories/RepairRepository.java
@@ -4,12 +4,16 @@ import com.coding.school.webapp.carRepair.Domain.Repair;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+
 @Repository
 public interface RepairRepository extends JpaRepository<Repair, Long> {
-    Repair findByDateTime (String dateTime);
+    Repair findByDateTime (Timestamp dateTime);
 
 
-    Repair findByID(Long ID);
+    default Repair findByID(Long ID) {
+        return null;
+    }
 
     Repair findByCost(double cost);
 
--- src/main/java/com/coding/school/webapp/carRepair/Services/RepairService.java
+++ src/main/java/com/coding/school/webapp/carRepair/Services/RepairService.java
@@ -2,6 +2,7 @@ package com.coding.school.webapp.carRepair.Services;
 
 import com.coding.school.webapp.carRepair.Domain.Repair;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public interface RepairService {
@@ -10,5 +11,5 @@ public interface RepairService {
 
     ArrayList<Repair> findAll();
 
-    Repair findByDateTime (String dateTime);
+    Repair findByDateTime (Timestamp dateTime);
 }
