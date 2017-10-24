7e22a4acf39394a72c40e8852f27e3394174883a
anvailas <anvailas@hotmail.com>
2017-10-24 19:22:12 +0300

    searchRepair validations

--- src/main/java/com/coding/school/webapp/carRepair/Model/SearchRepairForm.java
+++ src/main/java/com/coding/school/webapp/carRepair/Model/SearchRepairForm.java
@@ -2,12 +2,19 @@ package com.coding.school.webapp.carRepair.Model;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 public class SearchRepairForm {
 
+    private static final String VAT_PATTERN = "^[0-9]*$";
+
+    private static final String PLATE_PATTERN = "[ABEHIKMNOPTOXYZ]{3}-[0123456789]{4}";
+
+    @Pattern(regexp = VAT_PATTERN, message = "{register.vat.invalid")
     private String vat;
 
+    @Pattern(regexp = PLATE_PATTERN, message = "{register.plate.invalid")
     private String plateNumber;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
