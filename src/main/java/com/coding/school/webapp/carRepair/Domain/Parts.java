fc1800becca31f6a1c3889c8646b736a743b18dc
Maria Tsantaki <sdi1300276@di.uoa.gr>
2017-10-24 16:22:47 +0300

    Parts Entity

---
+++ src/main/java/com/coding/school/webapp/carRepair/Domain/Parts.java
@@ -0,0 +1,59 @@
+package com.coding.school.webapp.carRepair.Domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Parts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    @NotNull
+    @Enumerated(value = EnumType.STRING)
+    private PartsType type;
+
+    @NotNull
+    private double cost;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public PartsType getType() {
+        return type;
+    }
+
+    public void setType(PartsType type) {
+        this.type = type;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+
+    public enum PartsType {
+        TIRES,BRAKES,FENDER,WINDOWS
+    }
+
+    @ManyToOne(optional = false)
+    private Repair repair;
+
+    public Repair getRepair() {
+        return repair;
+    }
+
+    public void setRepair(Repair repair) {
+        this.repair = repair;
+    }
+}
